849a4c256e8a4d0d0e98caa8288637f64bcb1d29
Daniel Berger <dberger@globe.gov>
2011-03-01 10:21:45 -0700

    Refactored the test suite to use the declarative syntax and updated the tests in general.

--- test/test_net_tnsping.rb
+++ test/test_net_tnsping.rb
@@ -9,150 +9,205 @@ gem 'test-unit'
 
 require 'test/unit'
 require 'net/tnsping'
-include Net
 
 class TC_TNS_Ping < Test::Unit::TestCase
-   def self.startup
-      @@database = 'change_me' # Change to a valid host name
-      @@hostname = Socket.gethostname
-   end
+  def self.startup
+    @@database = 'xe'
+    @@username = 'hr'
+    @@password = 'hr'
+    @@hostname = Socket.gethostname
+  end
 
-   def setup
-      if @@database == 'change_me'
-         @tnsp = Ping::TNS.new(@@database, 'OCI8', @@hostname)
-      else
-         @tnsp = Ping::TNS.new(@@database)
-      end
-   end
-   
-   def test_version
-      assert_equal('1.3.1', Ping::TNS::VERSION)
-   end
+  def setup
+    @tnsp = Net::Ping::TNS.new(@@database)
+  end
 
-   def test_db_get
-      assert_respond_to(@tnsp, :db)
-      assert_nothing_raised{ @tnsp.db }
-      assert_equal(@@database, @tnsp.db)
-   end
+  test "version number is expected value" do
+    assert_equal('1.3.2', Net::Ping::TNS::VERSION)
+  end
 
-   def test_db_set
-      assert_respond_to(@tnsp, :db=)
-      assert_nothing_raised{ @tnsp.db = 'fubar' }
-      assert_equal('fubar', @tnsp.db)
-   end
+  test "database reader basic functionality" do
+    assert_respond_to(@tnsp, :database)
+    assert_nothing_raised{ @tnsp.database }
+    assert_equal(@@database, @tnsp.database)
+  end
 
-   def test_dsn_get
-      assert_respond_to(@tnsp, :dsn)
-      assert_nothing_raised{ @tnsp.dsn }
-      assert_equal("dbi:OCI8:#{@@database}", @tnsp.dsn)
-   end
+  test "db is an alias for database" do
+    assert_respond_to(@tnsp, :db)
+    assert_alias_method(@tnsp, :db, :database)
+  end
 
-   def test_dsn_set
-      assert_respond_to(@tnsp, :dsn=)
-      assert_nothing_raised{ @tnsp.dsn = 'dbi:OCI8:fubar' }
-      assert_equal('dbi:OCI8:fubar', @tnsp.dsn)
-   end
+  test "database writer basic functionality" do
+    assert_respond_to(@tnsp, :database=)
+    assert_nothing_raised{ @tnsp.database = 'fubar' }
+    assert_equal('fubar', @tnsp.database)
+  end
 
-   def test_port_get
-      assert_respond_to(@tnsp, :port)
-      assert_equal(1521, @tnsp.port)
-   end
+  test "dsn reader basic functionality" do
+    assert_respond_to(@tnsp, :dsn)
+    assert_nothing_raised{ @tnsp.dsn }
+    assert_equal("dbi:OCI8:#{@@database}", @tnsp.dsn)
+  end
 
-   def test_port_set
-      assert_respond_to(@tnsp, :port=)
-      assert_nothing_raised{ @tnsp.port = 1555 }
-      assert_equal(1555, @tnsp.port)
-   end
+  test "database_source_name is an alias for dsn" do
+    assert_respond_to(@tnsp, :database_source_name)
+    assert_alias_method(@tnsp, :database_source_name, :dsn)
+  end
 
-   def test_ports
-      assert_respond_to(@tnsp, :ports)
-      assert_kind_of(Array, @tnsp.ports)
-      assert(@tnsp.ports.length > 0)
-   end
+  test "dsn writer basic functionality" do
+    assert_respond_to(@tnsp, :dsn=)
+    assert_nothing_raised{ @tnsp.dsn = 'dbi:OCI8:fubar' }
+    assert_equal('dbi:OCI8:fubar', @tnsp.dsn)
+  end
 
-   def test_host_get
-      assert_respond_to(@tnsp, :host)
-      assert_equal(@@hostname, @tnsp.host)
-   end
+  test "port reader basic functionality" do
+    assert_respond_to(@tnsp, :port)
+    assert_equal(1521, @tnsp.port)
+  end
 
-   def test_host_set
-      assert_respond_to(@tnsp, :host=)
-      assert_nothing_raised{ @tnsp.host = 'fubar' }
-      assert_equal('fubar', @tnsp.host)
-   end
+  test "port writer basic functionality" do
+    assert_respond_to(@tnsp, :port=)
+    assert_nothing_raised{ @tnsp.port = 1555 }
+    assert_equal(1555, @tnsp.port)
+  end
 
-   def tests_hosts
-      assert_respond_to(@tnsp, :hosts)
-      assert_kind_of(Array, @tnsp.hosts)
-      assert(@tnsp.hosts.length > 0)
-   end
+  test "ports reader basic functionality" do
+    assert_respond_to(@tnsp, :ports)
+    assert_kind_of(Array, @tnsp.ports)
+    assert(@tnsp.ports.length > 0)
+  end
 
-   def test_tns_file_get
-      assert_respond_to(@tnsp, :tns_file)
-      omit_if(@@database == 'change_me', 'tns_file test skipped without real database')
-      assert_true(File.exist?(@tnsp.tns_file))
-   end
+  test "host reader basic functionality" do
+    assert_respond_to(@tnsp, :host)
+    assert_nil(@tnsp.host)
+  end
 
-   def test_tns_file_set
-      assert_respond_to(@tnsp, :tns_file=)
-      assert_nothing_raised{ @tnsp.tns_file = 'fu_tnsnames.ora' }
-      assert_equal('fu_tnsnames.ora', @tnsp.tns_file)
-   end
+  test "host writer basic functionality" do
+    assert_respond_to(@tnsp, :host=)
+    assert_nothing_raised{ @tnsp.host = 'fubar' }
+    assert_equal('fubar', @tnsp.host)
+  end
 
-   def test_oracle_home_get
-      assert_respond_to(@tnsp, :oracle_home)
-      assert_kind_of([String, NilClass], @tnsp.oracle_home)
-   end
+  test "hosts reader basic functionality" do
+    assert_respond_to(@tnsp, :hosts)
+    assert_kind_of(Array, @tnsp.hosts)
+    assert(@tnsp.hosts.length > 0)
+  end
 
-   def test_oracle_home_set
-      assert_respond_to(@tnsp, :oracle_home=)
-      assert_nothing_raised{ @tnsp.oracle_home = ENV['HOME'] }
-      assert_equal(ENV['HOME'], @tnsp.oracle_home)
-   end
+  test "there is no hosts writer method" do
+    assert_raise(NoMethodError){ @tnsp.hosts = %w[foo bar] }
+  end
 
-   def test_tns_admin_get
-      assert_respond_to(@tnsp, :tns_admin)
-      assert_kind_of([String, NilClass], @tnsp.tns_admin)
-   end
+  test "tns_file reader basic functionality" do
+    assert_respond_to(@tnsp, :tns_file)
+    assert_kind_of(String, @tnsp.tns_file)
+    assert_true(File.exist?(@tnsp.tns_file))
+  end
 
-   def test_tns_admin_set
-      assert_respond_to(@tnsp, :tns_admin=)
-      assert_nothing_raised{ @tnsp.tns_admin = ENV['HOME'] }
-      assert_equal(ENV['HOME'], @tnsp.tns_admin)
-   end
+  test "tns_file writer basic functionality" do
+    assert_respond_to(@tnsp, :tns_file=)
+    assert_nothing_raised{ @tnsp.tns_file = 'fu_tnsnames.ora' }
+    assert_equal('fu_tnsnames.ora', @tnsp.tns_file)
+  end
 
-   def test_ping_listener
-      assert_respond_to(@tnsp, :ping?)
-      assert_nothing_raised{ @tnsp.ping? }
-      omit_if(@@database == 'change_me', 'ping listener test skipped without real database')
-      assert_equal(true, @tnsp.ping?)
-   end
+  test "oracle_home reader basic functionality" do
+    assert_respond_to(@tnsp, :oracle_home)
+    assert_kind_of([String, NilClass], @tnsp.oracle_home)
+  end
 
-   def test_ping_database
-      assert_respond_to(@tnsp, :ping_database?)
-      assert_nothing_raised{ @tnsp.ping_database? }
-      omit_if(@@database == 'change_me', 'ping database test skipped without real database')
-      assert_equal(true, @tnsp.ping_database?)
-   end
+  test "ora_home is an alias for oracle_home" do
+    assert_respond_to(@tnsp, :ora_home)
+    assert_alias_method(@tnsp, :ora_home, :oracle_home)
+  end
 
-   def test_ping_all
-      assert_respond_to(@tnsp, :ping_all?)
-      assert_nothing_raised{ @tnsp.ping_all? }
-      omit_if(@@database == 'change_me', 'ping all test skipped without real database')
-      assert_equal(true, @tnsp.ping_all?)
-   end
+  test "oracle_home writer basic functionality" do
+    assert_respond_to(@tnsp, :oracle_home=)
+    assert_nothing_raised{ @tnsp.oracle_home = ENV['HOME'] }
+    assert_equal(ENV['HOME'], @tnsp.oracle_home)
+  end
 
-   def test_bad_db
-      assert_raises(Ping::TNS::Error){ Ping::TNS.new('bogus_db') }
-      @tnsp.port = 9999
-      assert_equal(false, @tnsp.ping_listener?)
-   end
+  test "ora_home= is an alias for oracle_home=" do
+    assert_respond_to(@tnsp, :ora_home=)
+    assert_alias_method(@tnsp, :ora_home=, :oracle_home=)
+  end
 
-   def teardown
-      @tnsp = nil
-   end
+  test "tns_admin reader basic functionality" do
+    assert_respond_to(@tnsp, :tns_admin)
+    assert_kind_of([String, NilClass], @tnsp.tns_admin)
+  end
 
-   def self.shutdown
-      @@database = nil
-   end
+  test "tns_admin writer basic functionality" do
+    assert_respond_to(@tnsp, :tns_admin=)
+    assert_nothing_raised{ @tnsp.tns_admin = ENV['HOME'] }
+    assert_equal(ENV['HOME'], @tnsp.tns_admin)
+  end
+
+  test "timeout reader basic functionality" do
+    assert_respond_to(@tnsp, :timeout)
+    assert_kind_of(Fixnum, @tnsp.timeout)
+  end
+
+  test "timeout writer basic functionality" do
+    assert_respond_to(@tnsp, :timeout=)
+    assert_nothing_raised{ @tnsp.timeout = 10 }
+    assert_equal(10, @tnsp.timeout)
+  end
+
+  test "driver reader basic functionality" do
+    assert_respond_to(@tnsp, :driver)
+    assert_kind_of(String, @tnsp.driver)
+  end
+
+  test "driver writer basic functionality" do
+    assert_respond_to(@tnsp, :driver=)
+    assert_nothing_raised{ @tnsp.driver = "oracle" }
+    assert_equal("oracle", @tnsp.driver)
+  end
+
+  test "ping_listener? basic functionality" do
+    assert_respond_to(@tnsp, :ping_listener?)
+    assert_nothing_raised{ @tnsp.ping_listener? }
+  end
+
+  test "ping_listener? returns a boolean value" do
+    assert_boolean(@tnsp.ping_listener?)
+  end
+
+  test "ping? is an alias for ping_listener?" do
+  end
+
+  test "ping_database? basic functionality" do
+    assert_respond_to(@tnsp, :ping_database?)
+    assert_nothing_raised{ @tnsp.ping_database? }
+  end
+
+  test "ping_database? returns a boolean value" do
+    assert_boolean(@tnsp.ping_database?)
+  end
+
+  test "ping_all? basic functionality" do
+    assert_respond_to(@tnsp, :ping_all?)
+    assert_nothing_raised{ @tnsp.ping_all? }
+  end
+
+  test "ping_all? returns a boolean value" do
+    assert_boolean(@tnsp.ping_all?)
+  end
+
+  test "ping fails against a bogus database" do
+    assert_raises(Net::Ping::TNS::Error){ Net::Ping::TNS.new('bogus_db') }
+    @tnsp.port = 9999
+    assert_false(@tnsp.ping_listener?)
+  end
+
+  def teardown
+    @tnsp = nil
+  end
+
+  def self.shutdown
+    @@username = nil
+    @@password = nil
+    @@database = nil
+    @@hostname = nil
+  end
 end
